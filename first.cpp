bf9b5431142c241f1fc7451506b062a60719d446
xianyuhf <xianyuhf@163.com>
2021-04-24 09:32:20 +0800

    Update first.cpp

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+}
+
+void test(){
+    int s=1;
+    signed int s1 = 2;
+    unsigned int s2 = 3;
+    short int s3 = 4;
+    long int s4 = 5;
+    unsigned long int  = 6;
+
+}
